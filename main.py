5d98012b748142d04e4901bcf0864c06277c67ff
zSMOGz <PavlovPavel94@yandex.ru>
2024-12-03 22:51:19 +0300

    В команду /text добавлено подбрасывание монетки, выполнен рефакторинг

--- main.py
+++ main.py
@@ -7,35 +7,40 @@ from enum import Enum
 from telebot.types import InlineKeyboardButton as Button
 import random
 
-TOKEN = ''
-MAX_WIDTH_STICKER = 512
-JOKES = ["""Почему комедии Аристофана были такие смешные? 
-Есть два варианта:  либо потому что ори и 100 фан, 
-либо потому что, кто не смеётся, будет аристофан.""",
-         """- Третий разряд по шахматам!
-- Чёрт, это самые бессмысленные пытки, которые я видел!
-- Ты прав, они не колятся. Неси топор!""",
-         """- Этот кроссовок - президент Франции.
-- Почему?
-- Мокр он...""",
-         """- Я занимаюсь пилатесом...
-- Если ты пила Tess, не значит, что ты занималась пилатесом""",
-         """Хотелось бы отдохнуть 100 лет, ведь я - чила век.""",
-         """Молоко ультравпастьтебевсованное."""]
-COMPLEMENTS = ["""Ты красивый, как мусор в лучах закатного солнца""",
-               """У тебя такая нежная кожа, как будто поженились поверхность
-луны и наждачка""",
-               """Твои волосы словно лунный свет - они повсюду""",
-               """Ты похож на пакет""",
-               """Видела тебя вчера в ресторане, ты так очаровательно пытался 
-купить просрочку"""]
+from config import (
+    TOKEN,
+    MAX_WIDTH_STICKER,
+    DEFAULT_ASCII_WIDTH,
+    DEFAULT_PIXEL_SIZE,
+    MAX_MESSAGE_LENGTH,
+    HEATMAP_COLD_COLOR,
+    HEATMAP_HOT_COLOR,
+    MESSAGES,
+    BUTTON_LABELS,
+    CALLBACKS,
+    CALLBACK_ANSWERS,
+    ASCII_SYMBOLS,
+    JOKES,
+    COMPLEMENTS,
+    COIN_RESULTS,
+    IMAGE_FORMATS,
+    IMAGE_PARAMS,
+    MESSAGE_PARAMS,
+    BOT_PARAMS,
+    STATE_KEYS
+)
+
+from typing import Dict, Optional, Tuple, Union, Callable, Any
+from telebot.types import Message, CallbackQuery, InlineKeyboardMarkup
 
 bot = telebot.TeleBot(TOKEN)
 
-user_states = {}  # тут будем хранить информацию о действиях пользователя
+# Типы для состояний пользователя
+UserState = Dict[int, Dict[str, str]]
+user_states: UserState = {}
 
 # набор символов из которых составляем изображение
-ascii_symbols_art = '@%#*+=-:. '
+ascii_symbols_art = ASCII_SYMBOLS
 
 
 class MirrorTypes(Enum):
@@ -43,7 +48,17 @@ class MirrorTypes(Enum):
     VERTICAL = 2
 
 
-def resize_image(image, new_width=100):
+def resize_image(image: Image.Image, new_width: int = 100) -> Image.Image:
+    """
+    Изменяет размер изображения с сохранением пропорций.
+    
+    Args:
+        image: Исходное изображение
+        new_width: Новая ширина изображения
+        
+    Returns:
+        Изображение с измененным размером
+    """
     width, height = image.size
     ratio = height / width
     new_height = int(new_width * ratio)
@@ -69,16 +84,21 @@ def grayify(image):
     return image.convert("L")
 
 
-def image_to_ascii(image_stream,
-                   new_width=40):
-    # Переводим в оттенки серого
-    image = Image.open(image_stream).convert('L')
-
-    # меняем размер сохраняя отношение сторон
+def image_to_ascii(image_stream: io.BytesIO, new_width: int = DEFAULT_ASCII_WIDTH) -> str:
+    """
+    Преобразует изображение в ASCII-арт.
+    
+    Args:
+        image_stream: Поток с изображением
+        new_width: Желаемая ширина ASCII-арта в символах
+        
+    Returns:
+        Строка с ASCII-артом
+    """
+    image = Image.open(image_stream).convert(IMAGE_PARAMS['COLOR_MODE_L'])
     width, height = image.size
     aspect_ratio = height / float(width)
-    new_height = int(
-        aspect_ratio * new_width * 0.55)  # 0,55 так как буквы выше чем шире
+    new_height = int(aspect_ratio * new_width * IMAGE_PARAMS['ASPECT_RATIO_MULTIPLIER'])
     img_resized = image.resize((new_width, new_height))
 
     img_str = pixels_to_ascii(img_resized)
@@ -103,8 +123,17 @@ def pixels_to_ascii(image):
 
 
 # Огрубляем изображение
-def pixelate_image(image,
-                   pixel_size):
+def pixelate_image(image: Image.Image, pixel_size: int) -> Image.Image:
+    """
+    Пикселизует изображение с заданным размером пикселя.
+    
+    Args:
+        image: Исходное изображение
+        pixel_size: Размер пикселя
+        
+    Returns:
+        Пикселизованное изображение
+    """
     image = image.resize(
         (image.size[0] // pixel_size, image.size[1] // pixel_size),
         Image.NEAREST
@@ -117,26 +146,60 @@ def pixelate_image(image,
 
 
 # Инвертирование изображения
-def invert_image(image):
+def invert_image(image: Image.Image) -> Image.Image:
+    """
+    Инвертирует цвета изображения.
+    
+    Args:
+        image: Исходное изображение
+        
+    Returns:
+        Изображение с инвертированными цветами
+    """
     return ImageOps.invert(image)
 
 
 # Отражение по горизонтали изображения
-def mirror_horizontal_image(image):
+def mirror_horizontal_image(image: Image.Image) -> Image.Image:
+    """
+    Отражает изображение по горизонтали.
+    
+    Args:
+        image: Исходное изображение
+        
+    Returns:
+        Отраженное по горизонтали изображение
+    """
     return image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
 
 
 # Отражение по вертикали изображения
-def mirror_vertical_image(image):
+def mirror_vertical_image(image: Image.Image) -> Image.Image:
+    """
+    Отражает изображение по вертикали.
+    
+    Args:
+        image: Исходное изображение
+        
+    Returns:
+        Отраженное по вертикали изображение
+    """
     return image.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
 
 
 # Тепловая карта изображения
-def convert_to_heatmap_image(image):
-    # Преобразуем изображение в оттенки серого
-    image = image.convert('L')
-
-    return ImageOps.colorize(image, (0, 0, 255), (255, 0, 0))
+def convert_to_heatmap_image(image: Image.Image) -> Image.Image:
+    """
+    Преобразует изображение в тепловую карту.
+    
+    Args:
+        image: Исходное изображение
+        
+    Returns:
+        Изображение в виде тепловой карты
+    """
+    image = image.convert(IMAGE_PARAMS['COLOR_MODE_L'])
+    return ImageOps.colorize(image, HEATMAP_COLD_COLOR, HEATMAP_HOT_COLOR)
 
 
 # Случайная шутка
@@ -149,55 +212,117 @@ def get_random_complements():
     return random.choice(COMPLEMENTS)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message,
-                 "Отправьте мне изображение, и я подумаю, что можно "
-                 + "сделать!")
+def flip_coin():
+    return random.choice(COIN_RESULTS)
 
+
+class ImageHandlers:
+    @staticmethod
+    def handle_photo(message: Message) -> None:
+        bot.reply_to(message, MESSAGES['PHOTO_RECEIVED'],
+                     reply_markup=get_photo_options_keyboard())
+        user_states[message.chat.id] = {
+            STATE_KEYS['PHOTO']: message.photo[-1].file_id
+        }
+
+    @staticmethod
+    def pixelate_and_send(message: Message) -> None:
+        if image := get_image_from_message(message):
+            pixelated = pixelate_image(image, 20)
+            send_image(message.chat.id, pixelated)
+
+
+class TextHandlers:
+    @staticmethod
+    def handle_start(message):
+        bot.reply_to(message, MESSAGES['START'])
+
+    @staticmethod
+    def handle_text_commands(message):
+        bot.reply_to(message,
+                     MESSAGES['CHOOSE_ACTION'],
+                     reply_markup=get_text_options_keyboard())
+
+    @staticmethod
+    def random_joke_and_send(message):
+        joke = get_random_joke()
+        bot.send_message(message.chat.id, joke)
+
+    @staticmethod
+    def random_complement_and_send(message):
+        complement = get_random_complements()
+        bot.send_message(message.chat.id, complement)
+
+    @staticmethod
+    def flip_coin_and_send(message):
+        result = flip_coin()
+        bot.send_message(message.chat.id, f"Монетка показала: {result}")
+
+
+class CallbackHandlers:
+    HandlerType = Tuple[Callable[[Message], None], str]
+    HandlersDict = Dict[str, HandlerType]
+
+    @staticmethod
+    def handle_callback(call: CallbackQuery) -> None:
+        handlers: CallbackHandlers.HandlersDict = {
+            "pixelate": (ImageHandlers.pixelate_and_send, MESSAGES['PIXELATE']),
+            "ascii": (ascii_and_send, MESSAGES['ASCII']),
+            "invert": (invert_and_send, MESSAGES['INVERT']),
+            "mirror_horizontal": (mirror_horizontal_and_send, MESSAGES['MIRROR_H']),
+            "mirror_vertical": (mirror_vertical_and_send, MESSAGES['MIRROR_V']),
+            "convert_to_heatmap": (convert_to_heatmap_and_send, MESSAGES['HEATMAP']),
+            "random_joke": (TextHandlers.random_joke_and_send, MESSAGES['JOKE']),
+            "random_component": (TextHandlers.random_complement_and_send, MESSAGES['COMPLEMENT']),
+            "flip_coin": (TextHandlers.flip_coin_and_send, MESSAGES['FLIP_COIN'])
+        }
+
+        if call.data in handlers:
+            handler, answer_text = handlers[call.data]
+            bot.answer_callback_query(call.id, answer_text)
+            handler(call.message)
+
+
+# Регистрация обработчиков
+@bot.message_handler(commands=['start', 'help'])
+def start_command(message):
+    TextHandlers.handle_start(message)
 
 @bot.message_handler(commands=['text'])
-def send_welcome(message):
-    bot.reply_to(message,
-                 "Выберите, что вы хотите сделать:",
-                 reply_markup=get_text_options_keyboard())
-
-
-def process_ascii_symbols_step(message):
-    global ascii_symbols_art
-    ascii_symbols_art = message.text
-    bot.send_message(message.chat.id, "Спасибо, теперь я знаю, чем рисовать "
-                     + "ASCII арты!")
-
+def text_command(message):
+    TextHandlers.handle_text_commands(message)
 
 @bot.message_handler(content_types=['photo'])
-def handle_photo(message):
-    bot.reply_to(message,
-                 "Я получил ваше изображение! "
-                 + "Пожалуйста, выберите, что вы хотите сделать:",
-                 reply_markup=get_photo_options_keyboard())
-    user_states[message.chat.id] = {'photo': message.photo[-1].file_id}
+def photo_message(message):
+    ImageHandlers.handle_photo(message)
 
-    msg = bot.reply_to(message,
-                       "Пожалуйста, введите символы для ASCII арт"
-                       + "(От самого тёмного до светлого (@%#*+=-:. ):")
-    bot.register_next_step_handler(msg, process_ascii_symbols_step)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    CallbackHandlers.handle_callback(call)
 
 
-def get_photo_options_keyboard():
+def process_ascii_symbols_step(message: Message) -> None:
+    """Обрабатывает ввод символов для ASCII арта"""
+    global ascii_symbols_art
+    ascii_symbols_art = message.text
+    bot.send_message(message.chat.id, MESSAGES['ASCII_SYMBOLS_THANKS'])
+
+
+def get_photo_options_keyboard() -> InlineKeyboardMarkup:
+    """Создает клавиатуру с опциями для фото"""
     keyboard = types.InlineKeyboardMarkup()
-    pixelate_btn = Button("Пикселизировать",
-                          callback_data="pixelate")
-    ascii_btn = Button("ASCII арт",
-                       callback_data="ascii")
-    invert_btn = Button("Инвертировать",
-                        callback_data="invert")
-    mirror_horizontal_btn = Button("Отразить по горизонтали",
-                                   callback_data="mirror_horizontal")
-    mirror_vertical_btn = Button("Отразить по вертикали",
-                                 callback_data="mirror_vertical")
-    convert_to_heatmap_btn = Button("Преобразовать в тепловую карту ",
-                                    callback_data="convert_to_heatmap")
+    pixelate_btn = Button(BUTTON_LABELS['PIXELATE'],
+                          callback_data=CALLBACKS['PIXELATE'])
+    ascii_btn = Button(BUTTON_LABELS['ASCII'],
+                       callback_data=CALLBACKS['ASCII'])
+    invert_btn = Button(BUTTON_LABELS['INVERT'],
+                        callback_data=CALLBACKS['INVERT'])
+    mirror_horizontal_btn = Button(BUTTON_LABELS['MIRROR_H'],
+                                   callback_data=CALLBACKS['MIRROR_H'])
+    mirror_vertical_btn = Button(BUTTON_LABELS['MIRROR_V'],
+                                 callback_data=CALLBACKS['MIRROR_V'])
+    convert_to_heatmap_btn = Button(BUTTON_LABELS['HEATMAP'],
+                                    callback_data=CALLBACKS['HEATMAP'])
     keyboard.add(pixelate_btn,
                  ascii_btn)
     keyboard.add(invert_btn)
@@ -209,83 +334,33 @@ def get_photo_options_keyboard():
 
 def get_text_options_keyboard():
     keyboard = types.InlineKeyboardMarkup()
-    random_joke_btn = Button("Случайная шутка",
-                             callback_data="random_joke")
-    random_component_btn = Button("Случайный комплемент",
-                                  callback_data="random_component")
+    random_joke_btn = Button(BUTTON_LABELS['JOKE'],
+                             callback_data=CALLBACKS['JOKE'])
+    random_component_btn = Button(BUTTON_LABELS['COMPLEMENT'],
+                                  callback_data=CALLBACKS['COMPLEMENT'])
+    flip_coin_btn = Button(BUTTON_LABELS['FLIP_COIN'],
+                           callback_data=CALLBACKS['FLIP_COIN'])
     keyboard.add(random_joke_btn, random_component_btn)
+    keyboard.add(flip_coin_btn)
     return keyboard
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    if call.data == "pixelate":
-        bot.answer_callback_query(call.id,
-                                  "Пикселизация изображения...")
-        pixelate_and_send(call.message)
-    elif call.data == "ascii":
-        bot.answer_callback_query(call.id,
-                                  "Преобразование изображений в ASCII "
-                                  + "арт...")
-        ascii_and_send(call.message)
-    elif call.data == "invert":
-        bot.answer_callback_query(call.id,
-                                  "Инвертирование изображения...")
-        invert_and_send(call.message)
-    elif call.data == "mirror_horizontal":
-        bot.answer_callback_query(call.id,
-                                  "Отражение изображения по "
-                                  + "горизонтали...")
-        mirror_horizontal_and_send(call.message)
-    elif call.data == "mirror_vertical":
-        bot.answer_callback_query(call.id,
-                                  "Отражение изображения по "
-                                  + "вертикали...")
-        mirror_vertical_and_send(call.message)
-    elif call.data == "convert_to_heatmap":
-        bot.answer_callback_query(call.id,
-                                  "Преобразование изображения в"
-                                  + "тепловую карту...")
-        convert_to_heatmap_and_send(call.message)
-    elif call.data == "random_joke":
-        bot.answer_callback_query(call.id,
-                                  "Случайная шутка...")
-        random_joke_and_send(call.message)
-    elif call.data == "random_component":
-        bot.answer_callback_query(call.id,
-                                  "Случайная комплемент...")
-        random_complement_and_send(call.message)
-
-
-def pixelate_and_send(message):
-    photo_id = user_states[message.chat.id]['photo']
-    file_info = bot.get_file(photo_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-
-    image_stream = io.BytesIO(downloaded_file)
-    image = Image.open(image_stream)
-    pixelated = pixelate_image(image, 20)
-
-    output_stream = io.BytesIO()
-    pixelated.save(output_stream, format="JPEG")
-    output_stream.seek(0)
-    bot.send_photo(message.chat.id, output_stream)
-
-
 def ascii_and_send(message):
-    photo_id = user_states[message.chat.id]['photo']
+    photo_id = user_states[message.chat.id][STATE_KEYS['PHOTO']]
     file_info = bot.get_file(photo_id)
     downloaded_file = bot.download_file(file_info.file_path)
 
     image_stream = io.BytesIO(downloaded_file)
     ascii_art = image_to_ascii(image_stream)
-    bot.send_message(message.chat.id,
-                     f"```\n{ascii_art}\n```",
-                     parse_mode="MarkdownV2")
+    bot.send_message(
+        message.chat.id,
+        f"{MESSAGE_PARAMS['CODE_BLOCK']}\n{ascii_art}\n{MESSAGE_PARAMS['CODE_BLOCK']}",
+        parse_mode=MESSAGE_PARAMS['PARSE_MODE']
+    )
 
 
 def invert_and_send(message):
-    photo_id = user_states[message.chat.id]['photo']
+    photo_id = user_states[message.chat.id][STATE_KEYS['PHOTO']]
     file_info = bot.get_file(photo_id)
     downloaded_file = bot.download_file(file_info.file_path)
 
@@ -308,7 +383,7 @@ def mirror_vertical_and_send(message):
 
 
 def mirror_and_send(message, mirror_type: MirrorTypes):
-    photo_id = user_states[message.chat.id]['photo']
+    photo_id = user_states[message.chat.id][STATE_KEYS['PHOTO']]
     file_info = bot.get_file(photo_id)
     downloaded_file = bot.download_file(file_info.file_path)
 
@@ -327,7 +402,7 @@ def mirror_and_send(message, mirror_type: MirrorTypes):
 
 
 def convert_to_heatmap_and_send(message):
-    photo_id = user_states[message.chat.id]['photo']
+    photo_id = user_states[message.chat.id][STATE_KEYS['PHOTO']]
     file_info = bot.get_file(photo_id)
     downloaded_file = bot.download_file(file_info.file_path)
 
@@ -341,14 +416,24 @@ def convert_to_heatmap_and_send(message):
     bot.send_photo(message.chat.id, output_stream)
 
 
-def random_joke_and_send(message):
-    joke = get_random_joke()
-    bot.send_message(message.chat.id, joke)
+def get_image_from_message(message: Message) -> Optional[Image.Image]:
+    """Получает изображение из сообщения"""
+    try:
+        photo_id = user_states[message.chat.id][STATE_KEYS['PHOTO']]
+        file_info = bot.get_file(photo_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        return Image.open(io.BytesIO(downloaded_file))
+    except Exception:
+        return None
 
 
-def random_complement_and_send(message):
-    complement = get_random_complements()
-    bot.send_message(message.chat.id, complement)
+def send_image(chat_id: int, image: Image.Image, 
+               format: str = IMAGE_FORMATS['JPEG']) -> None:
+    """Отправляет изображение в чат"""
+    output_stream = io.BytesIO()
+    image.save(output_stream, format=format)
+    output_stream.seek(0)
+    bot.send_photo(chat_id, output_stream)
 
 
-bot.polling(none_stop=True)
+bot.polling(none_stop=BOT_PARAMS['NONE_STOP'])
